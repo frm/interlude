fc0bc9362ea149fb989acf398d655a1aa24b2667
Fernando Mendes <devfrmendes@gmail.com>
2016-09-16 21:30:24 +0100

    :sunrise:

---
+++ interlude.rb
@@ -0,0 +1,56 @@
+require "httparty"
+
+module Interlude
+  class App
+    def initialize(step: 1, datasets: "datasets")
+      @step = step
+      @dataset_dir = datasets
+    end
+
+    def run
+      datasets.each do |set|
+        each_word set do |keyword|
+          if correct_answer?(keyword)
+            puts "FOUND IT! #{keyword}"
+            break
+          end
+        end
+      end
+    end
+
+    private
+
+    def datasets
+      @_datasets ||= Dir.glob("#{@dataset_dir}/*.txt")
+    end
+
+    def step
+      @_step ||= @step.to_s.rjust(2, "0")
+    end
+
+    def each_word(file, &block)
+      File.open(file, 'r') do |file|
+        file.each_line do |line|
+          block.call(line.strip)
+        end
+      end
+    end
+
+    def url(query)
+      answer = query.gsub(' ', '_').downcase
+      "http://quizchallenges.pixels.camp/ch3/#{step}-#{answer}.html"
+    end
+
+    def correct_answer?(query)
+      HTTParty.get(url query).code == 200
+    end
+  end
+end
+
+if __FILE__ == $0
+  if ARGV[0].nil?
+    Interlude::App.new.run
+  else
+    Interlude::App.new(step: ARGV[0].to_i).run
+  end
+end
